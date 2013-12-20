0917e6fa6c83beb13cec993f53c32f41a0c75639
root <root@fede2fb34959.(none)>
2013-12-20 07:24:02 +0000

    cleanup

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -3,14 +3,22 @@ var appControllers = angular.module('appControllers', []);
 appControllers.controller('GithubController',function($scope,$http) {
   //$scope.data = 'Before click';
   $scope.searchTerm='';
+  $scope.login = '';
+  $scope.avatar = '';
   $scope.findUser = function() {
    console.log($scope.searchTerm);
    $http.defaults.useXDomain = true;
    //Make github call
    $http.get('https://api.github.com/users/'+$scope.searchTerm).success(function(data) {
     $scope.data = data;
+    $scope.login = data.login;
+    $scope.avatar = data.avatar_url;
+    console.log($scope.login+' '+$scope.avatar);
    }).error(function(data) {
+     console.log('error');
      $scope.data = 'Something went wrong or invalid input';
+     $scope.login = '';
+     $scope.avatar = '';  
    });
   }
 });
